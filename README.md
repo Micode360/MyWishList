0a000314c2d7b8c7ee9eb941acfc884952fe4835
Micode360 <abayemiracle@gmail.com>
2022-09-21 16:19:26 +0100

    validation setup complete. markdown updated

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # My Wishlist
+
+![wishlist](./my-wishlist/public/img/wishlist.PNG)
+
+My wish list is a web application where a user can list down
+his or her wish, expectations and goals for future purposes.
+This is the front end section application.
+
+## Create the presentation
+
+After pulling code from github take the following steps:
+
+First enter into the my-wishlist folder:
+
+```bash
+cd my-wishlist
+```
+
+Run the command to install the packages.
+
+```bash
+yarn install
+```
+
+Then run the command to start up the local server. 
+
+```bash
+yarn run dev
+```
+
+And boom. the application runs on your local server.
+
+
+
--- my-wishlist/components/addWish.js
+++ my-wishlist/components/addWish.js
@@ -1,6 +1,9 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { useState } from "react"
+import axios from "axios"
 import { showModal } from "../store/utilsAction";
+import { addWish } from "../store/io";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -8,49 +11,85 @@ const AddWish = () => {
   let selector = useSelector(showModal);
   const dispatch = useDispatch();
 
-  const fileUpload = (e) => {
-    let type = e.target.files[0].type.split("/")[1];
+  //For file upload
+ 
 
-    if(type === 'png' || type === 'jpg' || type === 'pdf'){
-      console.log(type, "e");
-    }
-
-  }
-  
-
+  // With Formik
   const initialValues = {
-    _type: "wish",
     title: "",
     category: "",
-    description: "",
-    attachment: "",
-    visibility: ""
+    description: ""
   };
 
+  //Without Formik
+  let [others, setOthers ] = useState({
+    attachment: "",
+    attachmentError: "",
+    visibility: "",
+    visibilityError: ""
+  })
+
+
+  const fileUpload = (e) => {
+    let value = e.target.files[0];
+    let type = e.target.files[0].type.split("/")[1];
+    console.log(type, "type");
+    if (
+      type.toLowerCase() === "png" ||
+      type.toLowerCase() === "jpg" ||
+      type.toLowerCase() === "jpeg" ||
+      type.toLowerCase() === "webp" ||
+      type.toLowerCase() === "pdf"
+    ) {
+      
+      return setOthers({...others, attachment:value ,attachmentError: ""});
+    } else return setOthers({...others, attachmentError: "Invalid file format"});
+  };
+
+  const setVisibilty = (e) => {
+    setOthers({...others, visibility: e.target.value, visibilityError: ''});
+  }
 
   const validationSchema = Yup.object().shape({
     title: Yup.string().required(),
     description: Yup.string().required(),
-  })
+  });
 
-  const renderError = (errMessage) => <p className="text-red-500 text-xs normal-case">{ errMessage }</p>;
+  const renderError = (errMessage) => (
+    <p className="text-red-500 text-xs normal-case">{errMessage}</p>
+  );
 
   const onSubmit = (values) => {
-    console.log(values, "values");
+    if(others.visibility === ""){
+      console.log("in visibility");
+       setOthers({...others, visibilityError: 'Must choose visibility'})
+      return;
+    }
 
-    //alert(JSON.stringify(values, null, 2));
+    if(others.attachmentError != ""){
+      return;
+    }
+
+    let formData = new FormData();
+    formData.append('file', others.attachment);
+
+    let res = { ...values, attachment: others.attachment, visibility: others.visibility };
+    dispatch(addWish(res)); 
+
+    //If you are experiencing an error with serialized data when you are working on this again, please use axios here.
+    //The error is the image. Try to find a way to make the image give you a serialzed image.
   };
 
   if (selector.payload.utils.status) {
     return (
-      <Formik 
+      <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={async (values, { resetForm }) => {
           await onSubmit(values);
           resetForm();
         }}
-        >
+      >
         <Form className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
           <div className="relative w-auto my-6 mx-auto max-w-3xl">
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -95,7 +134,7 @@ const AddWish = () => {
                   ></Field>
 
                   <ErrorMessage name="description" render={renderError} />
-                  
+
                   <label className="text-xs text-gray-900 font-medium">
                     Add an Attachment. ({" "}
                     <span className="text-red-500">jpg</span>,{" "}
@@ -114,18 +153,28 @@ const AddWish = () => {
                     <span className="ml-2 text-xs normal-case">
                       Add an attachment
                     </span>
-                    <Field type="file" name="attachment" onChange={fileUpload} className="hidden" />
+                    <Field
+                      type="file"
+                      name="attachment"
+                      onChange={fileUpload}
+                      className="hidden"
+                    />
                   </label>
+
+                  {others.attachmentError != ""? <p className="text-red-500 text-xs normal-case">{ others.attachmentError }</p>: ''}
 
                   <div className="flex flex-col">
                     <label className="text-xs text-gray-900 font-medium">
                       Who can see this? <sup className="text-red-500">*</sup>
                     </label>
                     <div className="inline-flex">
-                      <button 
-                        className="w-fit flex items-center px-4 py-1 mr-1 bg-white text-blue rounded text-xs shadow-lg tracking-wide border border-blue cursor-pointer hover:bg-blue hover:text-black mb-2"
+                      <button
+                        type="button"
+                        value={"everyone"}
+                        onClick={setVisibilty}
+                        className={others.visibility === "everyone"? `border border-solid border-wish-blue w-fit flex items-center px-4 py-1 rounded text-xs shadow-lg tracking-wide cursor-pointer hover:bg-blue hover:text-black mb-2`: `border border-solid border-gray-200 w-fit flex items-center px-4 py-1 rounded text-xs shadow-lg tracking-wide cursor-pointer hover:bg-blue hover:text-black mb-2`}
                         name="visibility"
-                        >
+                      >
                         <svg
                           className="w-4 h-4 mr-1"
                           xmlns="http://www.w3.org/2000/svg"
@@ -147,10 +196,13 @@ const AddWish = () => {
                         </svg>
                         Everyone
                       </button>
-                      <button 
-                        className="w-fit flex items-center px-4 py-1 bg-white text-blue rounded text-xs shadow-lg tracking-wide border border-blue cursor-pointer hover:bg-blue hover:text-black mb-2"
+                      <button
+                        type="button"
+                        value={"only_me"}
+                        onClick={setVisibilty}
+                        className={others.visibility === "only_me"? `border border-solid border-wish-blue w-fit flex items-center px-4 py-1 rounded text-xs shadow-lg tracking-wide cursor-pointer hover:bg-blue hover:text-black mb-2`: `border border-solid border-gray-200 w-fit flex items-center px-4 py-1 rounded text-xs shadow-lg tracking-wide cursor-pointer hover:bg-blue hover:text-black mb-2`}
                         name="visibility"
-                        >
+                      >
                         <svg
                           className="w-4 h-4 mr-1"
                           xmlns="http://www.w3.org/2000/svg"
@@ -168,9 +220,14 @@ const AddWish = () => {
                         Only me
                       </button>
                     </div>
+
+                    {others.visibilityError != ""? <p className="text-red-500 text-xs normal-case">{ others.visibilityError }</p>: ''}
                   </div>
 
-                  <button type="submit" className="mt-5 tracking-wide font-semibold bg-wish-blue text-gray-100 w-full py-2 rounded-lg hover:bg-wish-blue-dark transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                  <button
+                    type="submit"
+                    className="mt-5 tracking-wide font-semibold bg-wish-blue text-gray-100 w-full py-2 rounded-lg hover:bg-wish-blue-dark transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
