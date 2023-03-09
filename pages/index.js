3ace8b37f507c56d2626703b21de4e0d19867e5a
Micode360 <abayemiracle@gmail.com>
2023-03-09 14:37:31 +0100

    gettiung data from json file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { getRequest } from "../lib/requests";
+//import { getRequest } from "../lib/requests";
 import { getUser } from "../store/user";
 import { useDispatch } from "react-redux";
 import Banner from "../components/banner";
@@ -9,11 +9,12 @@ import SearchBar from "../components/searchBar";
 import WishList from "../components/wishlist";
 import AddWish from "../components/addWish";
 import Description from "../components/description";
+import data from "./api/data.json"
 
-const Home = ({ user }) => {
+const Home = () => {
   const router = useRouter();
   let dispatch = useDispatch();
-  dispatch(getUser(user));
+  dispatch(getUser(data));
 
   return (
     <div>
@@ -40,16 +41,16 @@ const Home = ({ user }) => {
   );
 };
 
-export async function getServerSideProps() {
-  const user = await getRequest(
-    `${process.env.NEXT_PUBLIC_DORMAIN}/api/getuser`
-  );
+// export async function getServerSideProps() {
+//   const user = await getRequest(
+//     `${process.env.NEXT_PUBLIC_DORMAIN}/api/getuser`
+//   );
 
-  return {
-    props: {
-      user: user,
-    },
-  };
-}
+//   return {
+//     props: {
+//       user: user,
+//     },
+//   };
+// }
 
 export default Home;
